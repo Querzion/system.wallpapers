2ca7a8c2bc45a9a96f587998ee42b404a9323d0f
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2024-07-12 22:49:51 +0200

    Update move.wallpapers.sh

--- move.wallpapers.sh
+++ move.wallpapers.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
 
 # Full path to the git clone folder.
-FROM="/home/your-username/system.wallpapers"  # Change to the full path
+FOLDER="$HOME/system.wallpapers"
+
+H="$FOLDER/horisontal"  # Change to the full path
+V="$FOLDER/Vertical"
 # Most common fonts folder to use.
 TO="/usr/share/backgrounds"
 
 # Check if the FROM directory exists
-if [ ! -d "$FROM" ]; then
-    echo "Source directory $FROM does not exist."
+if [ ! -d "$H" ]; then
+    echo "Source directory $H does not exist."
+    exit 1
+fi
+
+# Check if the FROM directory exists
+if [ ! -d "$V" ]; then
+    echo "Source directory $V does not exist."
     exit 1
 fi
 
 # Moving the wallpapers
-sudo mv "$FROM"/* "$TO"
+sudo mv "$H"/* "$TO"
+sudo mv "$V"/* "$TO"
+
+# Directory to be removed
+FOLDER="/path/to/directory"
+
+# Check if the directory exists
+if [ -d "$FOLDER" ]; then
+    # Remove the directory and its contents
+    rm -r "$FOLDER"
+    echo "Directory $FOLDER and its contents have been removed."
+else
+    echo "Directory $FOLDER does not exist."
+fi
